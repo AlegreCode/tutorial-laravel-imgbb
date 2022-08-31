bbf2f282668c6da021e7479800185412b10c1cb6
Carlos Alberto Alegre <ca.alegre269@gmail.com>
2022-08-31 18:35:31 -0300

    agregado del script para el preview de la imagen en la vista home.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,11 +12,12 @@
         <section class="w-[30%]">
             <h1 class="text-3xl text-stone-300 text-center py-10">Tutorial de Laravel e ImgBB</h1>
             <form action="" method="post">
-                <img src="https://fakeimg.pl/350x350/?text=IMG" class="rounded-full w-[150px] h-auto mx-auto mb-10">                
+                <img src="https://fakeimg.pl/350x350/?text=IMG" class="w-[150px] h-auto mx-auto mb-10 rounded" id="preview-img">                
                                 
                 <label class="block mb-10">
                     <span class="sr-only">Choose File</span>
-                    <input type="file" class="block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-indigo-900 file:text-stone-300 hover:file:bg-sky-800 hover:file:cursor-pointer"/>
+                    <input type="file" class="block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-indigo-900 file:text-stone-300 hover:file:bg-sky-800 hover:file:cursor-pointer"
+                    id="fileinput"/>
                 </label>
 
                 <input type="text" name="name" id="name" placeholder="Name image" class="block w-full p-2 mb-10 bg-transparent border-indigo-900 border-b-2 outline-0 focus:border-sky-800">
@@ -24,5 +25,13 @@
             </form>
         </section>
     </main>
+    <script>
+        const fileinput = document.querySelector("#fileinput");
+        fileinput.addEventListener("change", function (e) { 
+            const target = e.target.files[0];
+            const previewimg = document.querySelector("#preview-img");
+            previewimg.src = URL.createObjectURL(target);
+         }, false);
+    </script>
 </body>
 </html>
