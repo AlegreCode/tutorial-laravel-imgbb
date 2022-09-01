d607b75cea8f7f5b2dec6c04baeba38b350b80e0
Carlos Alberto Alegre <ca.alegre269@gmail.com>
2022-09-01 18:12:48 -0300

    Creación de la galería y agragado de estilos.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,21 +8,31 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-    <main class="bg-slate-900 h-[100vh] flex justify-center items-center">
-        <section class="w-full max-w-[400px] p-3">
-            <h1 class="text-3xl text-stone-300 text-center py-10">Tutorial de Laravel e ImgBB</h1>
-            <form action="" method="post">
-                <img src="https://fakeimg.pl/350x350/?text=IMG" class="w-[150px] h-auto mx-auto mb-10 rounded" id="preview-img">                
-                                
-                <label class="block mb-10">
-                    <span class="sr-only">Choose File</span>
-                    <input type="file" class="block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-indigo-900 file:text-stone-300 hover:file:bg-sky-800 hover:file:cursor-pointer"
-                    id="fileinput"/>
-                </label>
-
-                <input type="text" name="name" id="name" placeholder="Name image" class="block w-full p-2 mb-10 bg-transparent border-indigo-900 border-b-2 outline-0 focus:border-sky-800">
-                <input type="submit" value="Enviar" class="text-center bg-indigo-900 hover:bg-sky-800 text-stone-300 p-3 rounded-xl cursor-pointer block w-full">
-            </form>
+    <main class="bg-slate-900">
+        <section class="h-[100vh] flex justify-center items-center">
+            <section class="w-full max-w-[400px] p-3">
+                <h1 class="text-3xl text-stone-300 text-center py-10">Tutorial de Laravel e ImgBB</h1>
+                <form action="" method="post">
+                    <img src="https://fakeimg.pl/350x350/?text=IMG" class="w-[150px] h-auto mx-auto mb-10 rounded" id="preview-img">                
+                                    
+                    <label class="block mb-10">
+                        <span class="sr-only">Choose File</span>
+                        <input type="file" class="block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-indigo-900 file:text-stone-300 hover:file:bg-sky-800 hover:file:cursor-pointer"
+                        id="fileinput"/>
+                    </label>
+    
+                    <input type="text" name="name" id="name" placeholder="Name image" class="block w-full p-2 mb-10 bg-transparent border-indigo-900 border-b-2 outline-0 focus:border-sky-800">
+                    <input type="submit" value="Enviar" class="text-center bg-indigo-900 hover:bg-sky-800 text-stone-300 p-3 rounded-xl cursor-pointer block w-full">
+                </form>
+            </section>
+        </section>
+        <section>
+            <h3 class="text-xl text-stone-300 text-center pb-4">GALERÍA</h3>
+        </section>
+        <section class="p-4 max-w-[960px] mx-auto grid grid-cols-1 gap-4 justify-items-center sm:grid-cols-2 lg:grid-cols-3">
+            @for ($i = 0; $i < 9; $i++)
+                <div class="bg-red-500 w-[300px] h-[300px] sm:even:justify-self-start sm:odd:justify-self-end lg:even:justify-self-auto lg:odd:justify-self-auto flex justify-center items-center text-white last:bg-sky-500">{{ $i + 1 }}</div>
+            @endfor
         </section>
     </main>
     <script>
