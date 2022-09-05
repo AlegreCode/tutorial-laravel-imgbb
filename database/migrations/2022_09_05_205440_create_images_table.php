ae1fd935fef16ea39d94ccec439379f7ea4e7c78
Carlos Alberto Alegre <ca.alegre269@gmail.com>
2022-09-05 18:18:31 -0300

    Modificación de archivo del modelo Image, agregado de campos fillables. Modificacíon de archivo de create_images_table, agregado de los campos title y url.

--- database/migrations/2022_09_05_205440_create_images_table.php
+++ database/migrations/2022_09_05_205440_create_images_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('images', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('url');
             $table->timestamps();
         });
     }
