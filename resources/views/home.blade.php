bc6a4244c7cc93498b9f272b21ddd8fd10b4b593
Carlos Alberto Alegre <ca.alegre269@gmail.com>
2022-09-06 18:18:02 -0300

    Retoques finales de estilos a la galería de imágenes.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,7 +22,7 @@
                         id="fileinput" name="fileimage"/>
                     </label>
     
-                    <input type="text" name="title" id="title" placeholder="Name image" class="block w-full p-2 mb-10 bg-transparent border-indigo-900 border-b-2 outline-0 focus:border-sky-800">
+                    <input type="text" name="title" id="title" placeholder="Name image" class="block w-full p-2 mb-10 bg-transparent border-indigo-900 border-b-2 outline-0 focus:border-sky-800 text-stone-300">
                     <input type="submit" value="Enviar" class="text-center bg-indigo-900 hover:bg-sky-800 text-stone-300 p-3 rounded-xl cursor-pointer block w-full">
                 </form>
             </section>
@@ -34,9 +34,9 @@
             <section class="p-4 max-w-[960px] mx-auto grid grid-cols-1 gap-4 justify-items-center sm:grid-cols-2 lg:grid-cols-3">
                 @foreach ($images as $image)
                     <div class="bg-red-500 w-[300px] h-[300px] sm:even:justify-self-start sm:odd:justify-self-end lg:even:justify-self-auto lg:odd:justify-self-auto flex justify-center items-center text-white last:bg-sky-500">
-                        <figure>
-                            <img src="{{ $image->url }}" alt="image">
-                            <figcaption>{{ $image->title}}</figcaption>
+                        <figure class="relative">
+                            <img src="{{ $image->url }}" alt="image" class="w-full h-auto">
+                            <figcaption class="absolute bg-black/75 bottom-0 left-0 w-full p-4">{{ $image->title}}</figcaption>
                         </figure>
                     </div>                    
                 @endforeach
